eb87510e63265afb4dc11294a5726588f1d0293d
yangmz0528 <yang.mingzi0528@gmail.com>
2024-07-24 14:03:06 +0800

    update variable-object/map

--- variable/compute.tf
+++ variable/compute.tf
@@ -20,7 +20,11 @@ resource "aws_instance" "compute" {
 
   root_block_device {
     delete_on_termination = true
-    volume_size           = var.ec2_volume_size
-    volume_type           = var.ec2_volume_type
+    volume_size           = var.ec2_volume_config.size
+    volume_type           = var.ec2_volume_config.volume
   }
+
+  tags = merge(var.additional_tags, {
+    MangedBy = "Terraform"
+  })
 }
--- variable/variables.tf
+++ variable/variables.tf
@@ -10,12 +10,30 @@ variable "ec2_instance_type" {
   }
 }
 
-variable "ec2_volume_size" {
-  type        = number
-  description = "size (GB) of root block volume attached tomanaged EC2 instances"
+variable "ec2_volume_config" {
+  type = object({
+    size = number
+    type = string
+  })
+  description = "size and type of the root block volume attached to managed EC2 instances"
+
+  default = {
+    size = 10
+    type = "gp3"
+  }
 }
 
-variable "ec2_volume_type" {
-  type        = string
-  description = "volume type of between gp2 and gp3"
+variable "additional_tags" {
+  type = map(string)
+  default = {}
 }
+
+#variable "ec2_volume_size" {
+#  type        = number
+#  description = "size (GB) of root block volume attached to managed EC2 instances"
+#}
+#
+#variable "ec2_volume_type" {
+#  type        = string
+#  description = "volume type of between gp2 and gp3"
+#}
