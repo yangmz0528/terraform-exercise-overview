3a518d8fa2c5d261b3ee87e09404d339e7689fca
yangmz0528 <yang.mingzi0528@gmail.com>
2024-07-25 14:56:05 +0800

    add create user-login-profile

--- 10_iam_users/users.tf
+++ 10_iam_users/users.tf
@@ -2,6 +2,32 @@ locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
 }
 
+# Create IAM user
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
+
+# Create login profile for users
+resource "aws_iam_user_login_profile" "users" {
+  for_each                = aws_iam_user.users
+  user                    = each.value.name
+  password_length         = 8
+  password_reset_required = true
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
 output "users" {
   value = local.users_from_yaml
+}
+
+output "passwords" {
+  value = {for user, user_login in aws_aws_iam_user_login_profile.users : user => user_login.password}
 }
