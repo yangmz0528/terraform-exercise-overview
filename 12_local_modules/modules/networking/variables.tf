60eb7624ad23f3a5ca2045016134f4932c950c93
yangmz0528 <yang.mingzi0528@gmail.com>
2024-08-13 14:22:00 +0800

    refactor(networking-tf-course: add variable description and adjust formatting

--- 12_local_modules/modules/networking/variables.tf
+++ 12_local_modules/modules/networking/variables.tf
@@ -12,6 +12,8 @@
 # }
 
 variable "vpc_config" {
+  description = "Contains the VPC configurations. More specifically, the required cidr_block and the VPC name."
+
   type = object({
     cidr_block = string
     name       = string
@@ -24,6 +26,14 @@ variable "vpc_config" {
 }
 
 variable "subnet_config" {
+  description = <<EOT
+  Accepts a map of subnet configurations. Each subnet configuration should contain
+
+  cidr_block : The CIDR block of the subnet.
+  public     : Whether the subnet should be public or not (default set to false).
+  az         : The availability zone where to deploy the subnet.
+  EOT
+
   type = map(object({ # use map here to allow users to create multiple subnets
     cidr_block = string
     public     = optional(bool, false)
